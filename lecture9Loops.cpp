28d8f5e386f9b1c3cbf2c44af0cf1642357a7b11
Developer9985 <94001072+Developer9985@users.noreply.github.com>
2024-06-19 16:42:57 +0530

    Add files via upload

---
+++ lecture9Loops.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+
+    // Demonsration of for loop 
+    cout<<"Printing 1 to 5 integers using for loop"<<endl;
+    for(int i = 0; i < 5; i++){
+        cout << i << " ";
+    }
+    cout << endl;
+
+    int i=0;
+ // Demonsration of while loop
+    while(i < 5){
+        cout << i << " ";
+        i++;
+    }
+    cout<<endl;
+
+    // Demonstration of do while loop
+
+    int k = 0;
+    do
+    {
+        cout<< k << " "; 
+        k++;
+    } while (k < 5);
+    
+
+
+
+
+}
